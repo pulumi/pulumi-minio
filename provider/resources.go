8aa9008d31cda2e91f3f0f076ea51cdbb9326200
Ian Wahbe <ian@wahbe.com>
2023-10-24 17:47:07 -0700

    Remove manual mappings

--- provider/resources.go
+++ provider/resources.go
@@ -19,13 +19,11 @@ import (
 	// embed is used to store bridge-metadata.json in the compiled binary
 	_ "embed"
 	"path/filepath"
-	"unicode"
 
 	"github.com/pulumi/pulumi-minio/provider/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
-	tfbridgetokens "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/tokens"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/tokens"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/terraform-provider-minio/terraform-provider-minio/minio"
 )
 
@@ -36,32 +34,6 @@ const (
 	// modules:
 	mainMod = "index" // the y module
 )
-
-// makeMember manufactures a type token for the package and the given module and type.
-func makeMember(mod string, mem string) tokens.ModuleMember {
-	return tokens.ModuleMember(mainPkg + ":" + mod + ":" + mem)
-}
-
-// makeType manufactures a type token for the package and the given module and type.
-func makeType(mod string, typ string) tokens.Type {
-	return tokens.Type(makeMember(mod, typ))
-}
-
-// makeDataSource manufactures a standard resource token given a module and resource name.  It
-// automatically uses the main package and names the file by simply lower casing the data source's
-// first character.
-func makeDataSource(mod string, res string) tokens.ModuleMember {
-	fn := string(unicode.ToLower(rune(res[0]))) + res[1:]
-	return makeMember(mod+"/"+fn, res)
-}
-
-// makeResource manufactures a standard resource token given a module and resource name.  It
-// automatically uses the main package and names the file by simply lower casing the resource's
-// first character.
-func makeResource(mod string, res string) tokens.Type {
-	fn := string(unicode.ToLower(rune(res[0]))) + res[1:]
-	return makeType(mod+"/"+fn, res)
-}
 
 // Provider returns additional overlaid schema and metadata associated with the provider..
 func Provider() tfbridge.ProviderInfo {
@@ -80,26 +52,6 @@ func Provider() tfbridge.ProviderInfo {
 		GitHubOrg:   "terraform-provider-minio",
 		Config:      map[string]*tfbridge.SchemaInfo{},
 		Version:     version.Version,
-		Resources: map[string]*tfbridge.ResourceInfo{
-			"minio_s3_bucket":                   {Tok: makeResource(mainMod, "S3Bucket")},
-			"minio_s3_bucket_notification":      {Tok: makeResource(mainMod, "S3BucketNotification")},
-			"minio_s3_bucket_policy":            {Tok: makeResource(mainMod, "S3BucketPolicy")},
-			"minio_s3_bucket_versioning":        {Tok: makeResource(mainMod, "S3BucketVersioning")},
-			"minio_s3_object":                   {Tok: makeResource(mainMod, "S3Object")},
-			"minio_iam_group":                   {Tok: makeResource(mainMod, "IamGroup")},
-			"minio_iam_group_membership":        {Tok: makeResource(mainMod, "IamGroupMembership")},
-			"minio_iam_group_policy":            {Tok: makeResource(mainMod, "IamGroupPolicy")},
-			"minio_iam_user":                    {Tok: makeResource(mainMod, "IamUser")},
-			"minio_iam_policy":                  {Tok: makeResource(mainMod, "IamPolicy")},
-			"minio_iam_user_policy_attachment":  {Tok: makeResource(mainMod, "IamUserPolicyAttachment")},
-			"minio_iam_group_policy_attachment": {Tok: makeResource(mainMod, "IamGroupPolicyAttachment")},
-			"minio_iam_group_user_attachment":   {Tok: makeResource(mainMod, "IamGroupUserAttachment")},
-			"minio_ilm_policy":                  {Tok: makeResource(mainMod, "IlmPolicy")},
-			"minio_iam_service_account":         {Tok: makeResource(mainMod, "IamServiceAccount")},
-		},
-		DataSources: map[string]*tfbridge.DataSourceInfo{
-			"minio_iam_policy_document": {Tok: makeDataSource(mainMod, "getIamPolicyDocument")},
-		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
@@ -135,8 +87,8 @@ func Provider() tfbridge.ProviderInfo {
 		MetadataInfo: tfbridge.NewProviderMetadata(metadata),
 	}
 
-	prov.MustComputeTokens(tfbridgetokens.SingleModule("minio_", mainMod,
-		tfbridgetokens.MakeStandard(mainPkg)))
+	prov.MustComputeTokens(tokens.SingleModule("minio_", mainMod,
+		tokens.MakeStandard(mainPkg)))
 	prov.MustApplyAutoAliases()
 	prov.SetAutonaming(255, "-")
 
